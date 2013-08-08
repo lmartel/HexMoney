0af86245e514ef6e2603669dd3fd309f5f034f05
Leo Martel <leo@lpm.io>
2013-08-07 23:50:55 -0700

    added parser

--- demo/demo.js
+++ demo/demo.js
@@ -14,32 +14,41 @@ function demo(){
 
     var prefix = "myHexGrid.addMany([ ";
     var suffix = " ]).drawAll();";
-    var emitCode = function(){
-
-        function miniVar(n){
-            return "_bg" + (n + 1);
-        }
-
-        var bgDeclarations = "";
-        for(var i = 0; i < bgs.length; i++){
-            bgDeclarations += "var " + miniVar(i) + " = " + bgs[i] + ";\n";
-        }
-        var coords = "";
-        for(var loc in selected){
-            if(!selected.hasOwnProperty(loc)) continue;
-            var bg = selected[loc];
-            coords += ",[" + loc + (bg ? "," + miniVar(bgs.indexOf(bg)) : "") + "]";
-        }
-        // Chop that first comma
-        coords = coords.replace(",", "");
-        d3.select(".result-code")
-            .property("value", bgDeclarations + prefix + coords + suffix);
-    };
     emitCode();
 
-    d3.select(".result-code").on("click", function(){
-        if(document.getSelection().toString().length === 0) this.select();
+    d3.select(".result-code").on("blur", function(){
+        var raw = this.value.split("\n");
+
+        selected = {};
+        count = 0;
+
+        var vars = []; // How meta
+        if(raw.length < 2) bgs = [];
+        else{
+            bgs = raw.slice(0, -1).map(function(line){
+                var quote = line.indexOf('"');
+                vars.push(line.split("var ")[1].split(" =")[0]);
+                return line.substr(quote, line.lastIndexOf('"') - quote + 1);
+            });
+        }
+
+        var cmd = raw[raw.length - 1];
+
+        for(var i = 0; i < bgs.length; i++){
+            // Replace all
+            cmd = cmd.split(vars[i]).join(bgs[i]);
+        }
+        cmd = cmd.substr(cmd.indexOf("["), cmd.lastIndexOf("]") - cmd.indexOf("[") + 1);
+        var data = JSON.parse(cmd);
+
+        demoGrid.setGlobalBackgroundColor("white").drawAll();
+        resultGrid.removeAll().drawAll();
+        for(var i = 0; i < data.length; i++){
+            var datum = data[i];
+            enableHex(demoGrid.get(datum[0], datum[1]), datum[2]);
+        }
     });
+
     d3.select("." + demoGrid.getDOMClass()).on("click", function(){
         var click = d3.mouse(this);
         var clicked = demoGrid.getAt(click[0], click[1]);
@@ -52,22 +61,51 @@ function demo(){
                 clicked.setBackgroundColor("white").draw();
             } else {
                 var bg = d3.select(".background-path").property("value");
-                selected[loc] = bg ? bg : false;
-                count++;
-                var hex = resultGrid.add(loc.x(), loc.y())
-                    .setPayload(loc.x() === 0 && loc.y() === 0 ? (new H$.Payload(null, new H$.Asset("asterisk.png", 16, 16))) : null);
-                if(bg){
-                    if(bgs.indexOf(bg) === -1) bgs.push(bg);
-                    hex.setBackgroundImage(bg);
-                    clicked.setBackgroundImage(bg);
-                } else {
-                    hex.setBackgroundColor("gray");
-                    clicked.setBackgroundColor("grey");
-                }
-                hex.draw();
-                clicked.draw();
+                enableHex(clicked, bg);
             }
             emitCode();
         }
     });
+
+    /* Helpers */
+    function miniVar(n){
+        return "_bg" + (n + 1);
+    }
+
+    function emitCode(){
+
+        var bgDeclarations = "";
+        for(var i = 0; i < bgs.length; i++){
+            bgDeclarations += 'var ' + miniVar(i) + ' = "' + bgs[i] + '";\n';
+        }
+        var coords = "";
+        for(var loc in selected){
+            if(!selected.hasOwnProperty(loc)) continue;
+            var bg = selected[loc];
+            coords += ",[" + loc + (bg ? "," + miniVar(bgs.indexOf(bg)) : "") + "]";
+        }
+        // Chop that first comma
+        coords = coords.replace(",", "");
+        d3.select(".result-code")
+            .property("value", bgDeclarations + prefix + coords + suffix);
+    }
+
+    function enableHex(clicked, bg){
+        var loc = clicked.getLocation();
+        selected[loc] = bg ? bg : false;
+        count++;
+        var hex = resultGrid.add(loc.x(), loc.y());
+        if(loc.x() === 0 && loc.y() === 0) hex.setPayload(new H$.Payload(null, new H$.Asset("asterisk.png", 16, 16)));
+        if(bg){
+            if(bgs.indexOf(bg) === -1) bgs.push(bg);
+            hex.setBackgroundImage(bg);
+            clicked.setBackgroundImage(bg);
+        } else {
+            hex.setBackgroundColor("gray");
+            clicked.setBackgroundColor("grey");
+        }
+        hex.draw();
+        clicked.draw();
+    }
+
 }
